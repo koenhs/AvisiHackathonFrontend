612d9be880f2f46ca7d7c488ea744183b74bb399
eduardo <eduardovanbenthum@gmail.com>
2024-10-02 22:08:56 +0200

    💄 changed styling tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,44 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '15px'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1300px',
+    },
+    fontFamily: {
+      primary: 'var(--font-ar-one-sans)'
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#FFF9EB',
+        accent: {
+          DEFAULT: '#17B800',
+          alt: '#24D400'
+        },
+        secondary: {
+          DEFAULT: '#1C2A48',
+          alt: '#16203A',
+        },
+        tertiary: {
+          DEFAULT: '#BF7E00',
+          alt: '#CC942F',
+          dark: '#895A00'
+        },
+        footer: {
+          DEFAULT: '#151515',
+        },
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
     },
   },
   plugins: [],
