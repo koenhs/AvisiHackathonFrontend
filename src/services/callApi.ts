541a46b51977053c8c29049b1db88e057fa333a0
koenhs <koenhs@gmail.com>
2024-10-03 14:32:26 +0200

    callApi

--- src/services/callApi.ts
+++ src/services/callApi.ts
@@ -2,7 +2,7 @@ const BASE_URL = "http://localhost:8080/api/";
 
 type DataType<T> = Record<string, T> | T;
 
-const callApi = async <T>(
+export const callApi = async <T>(
     endpoint: string,
     data: DataType<T> = {} as DataType<T>,
     method: string = 'GET'
