5ed3b1ddfacdd4d7cda76840ccb69c52dcb3b848
Sander Evers <183650570+SanderEvers123@users.noreply.github.com>
2024-10-03 22:07:00 +0200

    header, sidebar fixes

--- src/app/student-overzicht/Overzicht.tsx
+++ src/app/student-overzicht/Overzicht.tsx
@@ -25,18 +25,6 @@ export default function Overzicht() {
 
     return (
         <div className="flex h-screen">
-            {/* Sidebar */}
-            <div className="bg-gray-800 text-white w-64 p-4">
-                <h1 className="text-xl font-bold mb-4">User Name</h1>
-                <p className="text-gray-400">User Details</p>
-                {/*<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">*/}
-                {/*    */}
-                {/*</button>*/}
-                <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full bottom-60">
-                    logout
-                </button>
-                {/* You can add more user details here */}
-            </div>
 
             {/* Main content area */}
             <div className="flex-1 p-4 overflow-auto">
@@ -51,7 +39,7 @@ export default function Overzicht() {
                     {criterium.map((criteria) => (
                         <div key={criteria.id} className="bg-white p-6 rounded-lg shadow-md flex ">
                             <h3 className="text-xl font-semibold mb-2">{criteria.name}</h3>
-                            <button onClick={} className="bg-blue-500 float-end"></button>
+                            <button  className="bg-blue-500 float-end"></button>
                         </div>
                     ))}
                 </div>
