f63f54f47df45f4d5f02c3f8b41cfe28339fafe0
Bastiaan Hopman <Bastiaanhopman@gmail.com>
2024-10-03 12:10:12 +0200

    Initial table

--- src/app/student-overzicht/page.tsx
+++ src/app/student-overzicht/page.tsx
@@ -1,32 +1,50 @@
 export default function Page() {
     return (
         <div className="flex flex-row min-h-screen justify-center items-center">
-            <table className="table-auto" class={"basis-full"}>
-                <thead>
-                <tr>
-                    <th>Werkprocess</th>
-                    <th>Beschrijving</th>
-                    <th>Behaalde cijfer</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                    <td>Malcolm Lockyer</td>
-                    <td>1961</td>
-                </tr>
-                <tr>
-                    <td>Witchy Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
-                </tr>
-                </tbody>
-            </table>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Kerntaak
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Criterium
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Werkprocess
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Kerntaak
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                        <th scope="row"
+                            className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            test
+                        </th>
+                        <td className="px-6 py-4">
+                            Silver
+                        </td>
+                        <td className="px-6 py-4">
+                            Laptop
+                        </td>
+                        <td className="px-6 py-4">
+                            $2999
+                        </td>
+                        <td className="px-6 py-4">
+                            <a href="#"
+                               className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
 
     );
