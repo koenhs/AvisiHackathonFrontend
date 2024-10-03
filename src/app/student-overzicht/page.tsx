7f7ec9781f83e98ba9813de33dcc6844fb35439c
Bastiaan Hopman <Bastiaanhopman@gmail.com>
2024-10-03 19:19:48 +0200

    Update front-end example

--- src/app/student-overzicht/page.tsx
+++ src/app/student-overzicht/page.tsx
@@ -1,62 +1,10 @@
 "use client";
-
-import {CriteriumDto} from "@/dtos/criteriumDto";
-import {callApi} from "@/services/callApi";
-import {useEffect, useState} from "react";
-
-export default function Page() {
-    const [criterium, setCriterium] = useState<CriteriumDto[]>([]);
-
-    useEffect(() => {
-        // Function to fetch data
-        const getAllCriterium = async () => {
-            try {
-                const response = await callApi<CriteriumDto[]>("StudentOverzicht/CriteriaAll");
-                const data = await response.json();
-                setCriterium(data);  // Update the state with fetched data
-            } catch (error) {
-                console.error("Error fetching criterium data:", error);
-            }
-        };
-        // Automatically call the function inside useEffect
-        getAllCriterium();
-    }, []);
+import Overzicht from "@/app/student-overzicht/Overzicht";
 
 
-    return (
-        <div className="flex h-screen">
-            {/* Sidebar */}
-            <div className="bg-gray-800 text-white w-64 p-4">
-                <h1 className="text-xl font-bold mb-4">User Name</h1>
-                <p className="text-gray-400">User Details</p>
-                {/* You can add more user details here */}
-            </div>
 
-            {/* Main content area */}
-            <div className="flex-1 p-4 overflow-auto">
-                <h2 className="text-2xl font-semibold mb-4">Data Table</h2>
-                <table className="min-w-full bg-white border border-gray-300">
-                    <thead>
-                    <tr className="bg-gray-200">
-                        <th className="border px-4 py-2">Criterium</th>
-                        <th className="border px-4 py-2">Kerntaak</th>
-                        <th className="border px-4 py-2">Werkprocess</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {criterium.map(criteria => (
-                        <tr key={criteria.id} className="border-b">
-                            <td className="border px-4 py-2 text-center">{criteria.id}</td>
-                            <td className="border px-4 py-2 text-center">{criteria.name}</td>
-                            <td className="border px-4 py-2 text-center">{criteria.id}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-    );
+export default function page (){
+    return <Overzicht/>
 }
 
 
